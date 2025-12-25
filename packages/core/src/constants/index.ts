715c6636411293eb7be6a039c515f665460db925
nichosta <npcosta@wm.edu>
2025-12-24 20:37:53 -0500

    Forgot index.ts

--- packages/core/src/constants/index.ts
+++ packages/core/src/constants/index.ts
@@ -2,13 +2,13 @@
  * Emerald memory addresses and lookup tables
  */
 
-export * from './ability_map';
-export * from './event_object_map_emerald';
-export * from './item_map';
-export * from './map_map_emerald';
-export * from './metatile_behaviors_map';
-export * from './moves_map';
-export * from './savestate_flag_map_emerald';
-export * from './species_map';
-export * from './text_character_map';
+export * from './abilityMap';
+export * from './eventObjectMap';
+export * from './itemMap';
+export * from './overworldMapMap';
+export * from './metatileBehaviorsMap';
+export * from './movesMap';
+export * from './savestateFlagMap';
+export * from './speciesMap';
+export * from './textCharacterMap';
 export * from './taskFunctions';
